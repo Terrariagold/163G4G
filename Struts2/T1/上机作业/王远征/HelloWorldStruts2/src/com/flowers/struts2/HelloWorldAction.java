ac8902bbd94fa1c2588e1e76f7b31f1498e34dc6
Terrariagold <g1244882348@gmail.com>
2017-12-06 16:04:05 +0800

    1,提交上机作业
    Signed-off-by: Terrariagold <g1244882348@gmail.com>

---
+++ Struts2/T1/上机作业/王远征/HelloWorldStruts2/src/com/flowers/struts2/HelloWorldAction.java
@@ -0,0 +1,37 @@
+package com.flowers.struts2;
+
+public class HelloWorldAction{
+	private String uname;
+	private String upwd;
+
+	public String execute() throws Exception {
+		if (uname == "tom" || uname.equals("tom")) {
+			System.out.println("³É¹¦");
+			return "success";
+		}else {
+			System.out.println("Ê§°Ü");
+			return "input";
+		}
+	}
+	
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpwd() {
+		return upwd;
+	}
+	public void setUpwd(String upwd) {
+		this.upwd = upwd;
+	}
+	public HelloWorldAction(String uname, String upwd) {
+		super();
+		this.uname = uname;
+		this.upwd = upwd;
+	}
+	public HelloWorldAction() {
+		super();
+	}
+}
